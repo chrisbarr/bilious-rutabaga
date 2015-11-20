b2d01f03c2cce8342f11139279870156c0ebc9c9
Chris Barr <chris.barr@mendeley.com>
2015-11-20 15:17:37 +0000

    0.2.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'bucket_lister',
     packages = find_packages(),
-    version = '0.2.2',
+    version = '0.2.3',
     description = 'List S3 buckets for an account',
     author = 'Chris Barr',
     author_email = 'chris.barr@ntlworld.com',
